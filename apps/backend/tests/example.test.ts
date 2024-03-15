b4729ee896a4581b9e2cc5c8df1b9301644d8f4a
Conner <81334211+ARealConner@users.noreply.github.com>
2024-03-15 02:54:45 -0400

    Add describe to example test case.

--- apps/backend/tests/example.test.ts
+++ apps/backend/tests/example.test.ts
@@ -1,7 +1,13 @@
-import { expect, test } from "vitest";
+import { expect, test, describe } from "vitest";
 function sum(a: number, b: number) {
   return a + b;
 }
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+describe("sum", () => {
+  test("adds 1 + 2 to equal 3", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+  test("adds 2 + 2 to equal 4", () => {
+    expect(sum(2, 2)).toBe(4);
+  });
 });
