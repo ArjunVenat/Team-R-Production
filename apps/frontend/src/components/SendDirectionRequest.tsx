aed59d84c61878fb9c6ee6f49f89910dae2ec0e2
Hubert Liu <hubertliu100@gmail.com>
2024-03-31 16:56:06 -0400

    uses GET request instead of POST request to find path

--- apps/frontend/src/components/SendDirectionRequest.tsx
+++ apps/frontend/src/components/SendDirectionRequest.tsx
@@ -9,12 +9,22 @@ export async function sendDirections(request: Directions) {
   console.log(data);
   //sends a post request the /api/high-score
   //ToDo: change api
-  const res = await axios.post("/api/map/pathfind", data, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+
+  // const res = await axios.get("/api/map/pathfind", data, {
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  // });
+    const res = await axios.get('/api/map/pathfind', {
+       params: {
+            startNodeID: request.start,
+            endNodeID: request.end,
+        }
+    });
+  if (res.status == 204) {
+      console.log("no path found");
+  }
   if (res.status == 200) {
-    console.log("submitted request");
+    console.log(res.data);
   }
 }
