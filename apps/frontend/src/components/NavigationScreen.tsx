393ec4efd230a2537516eb827dc058c46173b2cd
Jessie H <jessie.r.h13@gmail.com>
2024-03-30 21:13:16 -0400

    enter navigation start and end bones

--- apps/frontend/src/components/NavigationScreen.tsx
+++ apps/frontend/src/components/NavigationScreen.tsx
@@ -1,4 +1,72 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import { Button } from "@mui/material";
+import { useState } from "react";
+import { Directions } from "./Directions.tsx";
+import { sendDirections } from "./SendDirectionRequest.tsx";
 
+import { ChangeEvent } from "react";
+
+// interface Location {
+//     label: string;
+//     nodeID: string;
+// }
+
+const Locations = [
+  "Anesthesia Conf Floor L1",
+  "Medical Records Conference Room Floor L1",
+  "Abrams Conference Room",
+  "Day Surgery Family Waiting Floor L1",
+];
 export default function NavigationScreen() {
+  const [start, setStart] = useState("");
+  const [end, setEnd] = useState("");
+  const [points, setPoints] = useState<Directions>({ start: "", end: "" });
 
+  function getDirections() {
+    setPoints({ start: start, end: end });
+    sendDirections(points).then();
+  }
+
+  return (
+    <>
+      <h1> Enter your start and end locations</h1>
+      <div className="container">
+        <Autocomplete
+          value={start}
+          onChange={(event: ChangeEvent<unknown>, getStart: string | null) => {
+            return setStart(getStart);
+          }}
+          disablePortal
+          id="combo-box-start"
+          options={Locations}
+          sx={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField {...params} label="Start Location" />
+          )}
+        />
+
+        <Autocomplete
+          value={end}
+          onChange={(event: ChangeEvent<unknown>, getEnd: string | null) => {
+            setEnd(getEnd);
+          }}
+          disablePortal
+          id="combo-box-end"
+          options={Locations}
+          sx={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField {...params} label="End Location" />
+          )}
+        />
+      </div>
+
+      <div className="form-item">
+        <Button variant="contained" color="success" onClick={getDirections}>
+          Get Directions
+        </Button>
+      </div>
+    </>
+  );
 }
--- apps/frontend/src/components/SuccessAlert.tsx
+++ apps/frontend/src/components/SuccessAlert.tsx
@@ -4,26 +4,26 @@ import Slide from "@mui/material/Slide";
 import { Alert } from "@mui/material";
 
 export default function SuccessAlert() {
-    const [state, setState] = React.useState(true);
+  const [state, setState] = React.useState(true);
 
-    return (
-        <>
-            <Slide direction={"up"} in={state} mountOnEnter unmountOnExit>
-                <Snackbar
-                    open={state}
-                    autoHideDuration={3000}
-                    onClose={() => setState(false)}
-                >
-                    <Alert
-                        onClose={() => setState(false)}
-                        severity="success"
-                        variant="filled"
-                        sx={{ width: "100%" }}
-                    >
-                        This is a success Alert inside a Snackbar!
-                    </Alert>
-                </Snackbar>
-            </Slide>
-        </>
-    );
+  return (
+    <>
+      <Slide direction={"up"} in={state} mountOnEnter unmountOnExit>
+        <Snackbar
+          open={state}
+          autoHideDuration={3000}
+          onClose={() => setState(false)}
+        >
+          <Alert
+            onClose={() => setState(false)}
+            severity="success"
+            variant="filled"
+            sx={{ width: "100%" }}
+          >
+            This is a success Alert inside a Snackbar!
+          </Alert>
+        </Snackbar>
+      </Slide>
+    </>
+  );
 }
