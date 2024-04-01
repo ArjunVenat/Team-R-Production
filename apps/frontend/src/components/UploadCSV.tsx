ce012b61cf1305edfcaa075a8bd95201440e14a4
avstoyanov <avstoyanov@gmail.com>
2024-04-01 19:55:27 -0400

    experimenting with mui button

--- apps/frontend/src/components/UploadCSV.tsx
+++ apps/frontend/src/components/UploadCSV.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import axios from 'axios';
 import SuccessAlert from "./SuccessAlert.tsx";
+import {Button, styled} from "@mui/material";
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+
+
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+});
+
 
 export default function UploadCSV() {
     // a local state to store the currently selected file.
@@ -32,6 +48,16 @@ export default function UploadCSV() {
         <form onSubmit={handleSubmit}>
             <input type="file" onChange={handleFileSelect}/><br/>
             <button type="submit">Upload File</button>
+            <Button
+                component="label"
+                role={undefined}
+                variant="contained"
+                tabIndex={-1}
+                startIcon={<CloudUploadIcon />}
+            >
+                Upload file
+                <VisuallyHiddenInput type="file" />
+            </Button>
         </form>
     );
 };
