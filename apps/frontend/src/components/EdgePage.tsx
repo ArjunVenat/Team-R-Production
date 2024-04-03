d286dde1611fc87523f6e43831fb59f6f8c12c0a
ArjunVenat <arjunvenat@gmail.com>
2024-04-03 02:56:49 -0400

    fixed minor inconsistency

--- apps/frontend/src/components/EdgePage.tsx
+++ apps/frontend/src/components/EdgePage.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import EdgeTable from "./Edges.tsx";
 import SideBar from "./SideBar.tsx";
-// import { Box } from "@mui/material"; // Assuming NodeTable component is defined in NodeTable.tsx
+import { Box } from "@mui/material"; // Assuming NodeTable component is defined in NodeTable.tsx
 
 const EdgeTablePage = () => {
   return (
-      <div className="flex">
+      <Box display="flex">
           <SideBar />
           <div className="overflow-y-auto h-screen">
             <EdgeTable />
           </div>
-      </div>
+      </Box>
   );
 };
 
