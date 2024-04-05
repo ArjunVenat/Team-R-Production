47c6e7f00a559571c30350fa3a3337f08aac5823
Hubert Liu <hubertliu100@gmail.com>
2024-04-05 11:56:26 -0400

    created interface for heap

---
+++ apps/backend/src/heap.ts
@@ -0,0 +1,27 @@
+import graphNode from "./graph.ts";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class minHeap {
+  constructor() {
+    // do something idk
+  }
+
+  // Public methods
+  /* eslint-disable */
+  insert(node: graphNode, weight: number) {}
+
+  delete(node: graphNode) {}
+
+  // Returns the value at the top but does not remove it
+  peek(): graphNode {
+    // TODO: make this not a dummy return
+    return new graphNode();
+  }
+
+  // Returns the value at the top AND removes it
+  pop(): graphNode {
+    // TODO: make this not a dummy return
+    return new graphNode();
+  }
+  /* eslint-enable */
+}
