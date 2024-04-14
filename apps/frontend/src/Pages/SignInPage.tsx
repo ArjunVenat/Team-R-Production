84c50d7c937ddf615423a249c0a3f2d7fa70d485
LaurenEHarrison <leharrison@wpi.edu>
2024-04-13 20:57:59 -0400

    working on hero page

--- apps/frontend/src/Pages/SignInPage.tsx
+++ apps/frontend/src/Pages/SignInPage.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import bwh from "../assets/bwh.jpeg";
+// import bwh from "../assets/bwh.jpeg";
 import { useAuth0 } from "@auth0/auth0-react";
+
+import blurHall from "../assets/hero/blurHall.png";
+import bwhoutside from "../assets/hero/bwhoutside.png";
+import hall from "../assets/hero/hall.png";
+import outsidebwh from "../assets/hero/outsidebwh.png";
 
 const UserTypeList = motion.div;
 const UserTypeButton = motion.button;
@@ -9,50 +15,98 @@ const UserTypeButton = motion.button;
 function SignInPage() {
   const { loginWithRedirect } = useAuth0();
 
+  const [currentSlide, setCurrentSlide] = useState(1);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide % 4) + 1);
+    }, 5000); // Change slide every 5 seconds
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <>
+    <div className="relative">
+      {/* Carousel */}
       <div
-        className="flex justify-center items-center bg-cover bg-center bg-no-repeat"
-        style={{
-          backgroundImage: `url(${bwh})`,
-          width: "100vw",
-          height: "100vh",
-        }}
+        id="default-carousel"
+        className="bg-gray-600 absolute top-0 left-0 w-full h-full"
+        data-carousel="slide"
       >
-        <div
-          className="grid place-items-center bg-white bg-opacity-75 rounded-xl border-solid border-4 border-gray-300"
-          style={{ height: "50vh", width: "40vw" }}
-        >
-          <AnimatePresence>
-            {
-              <UserTypeList
-                initial={{ opacity: 0, y: -50 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -50 }}
-                transition={{ duration: 0.5 }}
-                className="flex flex-col items-center"
+        <AnimatePresence>
+          <div className="relative h-full overflow-hidden rounded-lg transition-transform">
+            {currentSlide === 1 && (
+              <motion.img
+                key={blurHall}
+                src={blurHall}
+                className="absolute inset-0 w-full h-full object-cover ease-in-out"
+                alt="..."
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              />
+            )}
+            {currentSlide === 2 && (
+              <motion.img
+                key={bwhoutside}
+                src={bwhoutside}
+                className="absolute inset-0 w-full h-full object-cover ease-in-out"
+                alt="..."
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              />
+            )}
+            {currentSlide === 3 && (
+              <motion.img
+                key={hall}
+                src={hall}
+                className="absolute inset-0 w-full h-full object-cover ease-in-out"
+                alt="..."
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              />
+            )}
+            {currentSlide === 4 && (
+              <motion.img
+                key={outsidebwh}
+                src={outsidebwh}
+                className="absolute inset-0 w-full h-full object-cover ease-in-out"
+                alt="..."
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              />
+            )}
+          </div>
+        </AnimatePresence>
+      </div>
+
+      {/* Sign-in Content */}
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="grid place-items-center bg-white bg-opacity-75 rounded-xl border-solid border-4 border-gray-300 z-20">
+          <div>
+            <UserTypeList className="flex flex-col items-center">
+              <h1 id="sign_in_h1" className="text-4xl">
+                Welcome
+              </h1>
+              <UserTypeButton
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                onClick={() =>
+                  loginWithRedirect({
+                    appState: { returnTo: "/home" },
+                  })
+                }
+                className="m-10 p-4 w-48 bg-primary text-white font-bold rounded-lg hover:bg-[#012d5a]"
               >
-                <h1 id="sign_in_h1" className="text-4xl">
-                  Welcome
-                </h1>
-                <UserTypeButton
-                  whileHover={{ scale: 1.1 }}
-                  whileTap={{ scale: 0.9 }}
-                  onClick={() =>
-                    loginWithRedirect({
-                      appState: { returnTo: "/home" },
-                    })
-                  }
-                  className="m-10 p-4 w-48 bg-primary text-white font-bold rounded-lg hover:bg-[#012d5a]"
-                >
-                  Log In
-                </UserTypeButton>
-              </UserTypeList>
-            }
-          </AnimatePresence>
+                Log In
+              </UserTypeButton>
+            </UserTypeList>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
