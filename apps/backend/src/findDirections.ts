78f6230271277d3785d5e788b105ded0c62658ba
Brannon Henson <bmhenson@icloud.com>
2024-04-19 16:04:23 -0400

    enum used for algo context

--- apps/backend/src/findDirections.ts
+++ apps/backend/src/findDirections.ts
@@ -5,18 +5,30 @@ export class Directions {
     this.path = path;
   }
 
-  private getAngle(point1: GraphNode, center: GraphNode, point3: GraphNode) {
+  public getAngle(point1: GraphNode, center: GraphNode, point3: GraphNode) {
     const p1c = point1.getDistance(center);
     const p3c = point3.getDistance(center);
     console.log("p1c", p1c);
     console.log("p3c", p3c);
-    const angle = 0;
+    const x1: number = point1.x - center.x;
+    const y1: number = point1.y - center.y;
+    const x2: number = point3.x - center.x;
+    const y2: number = point3.y - center.y;
+
+    let angle = Math.atan2(y1, x1) - Math.atan2(y2, x2);
+    angle = angle * (180 / Math.PI);
+
+    if (angle < 0) {
+      angle += 360;
+    }
+
     return angle;
   }
 
   public getAngles() {
     const path = this.path;
     const angles: number[] = [];
+    const directions: string[] = [];
 
     for (let i = 0; i < path.length; i++) {
       const point1 = path[i];
@@ -29,6 +41,23 @@ export class Directions {
         console.log("findDirections", angle);
       }
     }
-    return angles;
+    for (let i = 0; i < angles.length; i++) {
+      if (angles[i] < 60) {
+        directions.push("sharp left");
+      } else if (angles[i] >= 60 && angles[i] <= 120) {
+        directions.push("left");
+      } else if (angles[i] > 120 && angles[i] <= 165) {
+        directions.push("slight left");
+      } else if (angles[i] > 165 && angles[i] <= 195) {
+        directions.push("straight");
+      } else if (angles[i] > 195 && angles[i] <= 240) {
+        directions.push("slight right");
+      } else if (angles[i] > 240 && angles[i] <= 300) {
+        directions.push("right");
+      } else if (angles[i] >= 300 && angles[i] < 360) {
+        directions.push("sharp right");
+      }
+    }
+    return directions;
   }
 }
