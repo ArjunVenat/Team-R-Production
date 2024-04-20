fc5b81b4e7f3caa0a52b80c59d21e2a3e12249fd
Artem Frenk <97616103+afrenkai@users.noreply.github.com>
2024-04-20 19:39:57 -0400

    fixed order of sidebar pages oops

--- apps/frontend/src/components/SideBar.tsx
+++ apps/frontend/src/components/SideBar.tsx
@@ -91,8 +91,8 @@ export default function Sidebar() {
     nodes_edges,
     // uploadCSV,
     downloadCSV,
-    logoutOption,
     stats,
+    logoutOption,
   ];
 
   const location = useLocation();
