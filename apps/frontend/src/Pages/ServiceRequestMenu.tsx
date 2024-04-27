fcd45ec15f6bacee045218e13aab54117defa582
Jessie H <jessie.r.h13@gmail.com>
2024-04-27 12:41:49 -0400

    added images and removed background from service request menu

--- apps/frontend/src/Pages/ServiceRequestMenu.tsx
+++ apps/frontend/src/Pages/ServiceRequestMenu.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button, Box, Modal, Card } from "@mui/material";
+import { Button, Box, Modal, Card, Typography } from "@mui/material";
 import ServiceRequestLog from "../components/FullServiceRequest.tsx";
 
-import LocalFloristIcon from "@mui/icons-material/LocalFlorist";
-import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
-import BuildIcon from "@mui/icons-material/Build";
-import VaccinesIcon from "@mui/icons-material/Vaccines";
-import MonitorHeartIcon from "@mui/icons-material/MonitorHeart";
-import CasinoIcon from "@mui/icons-material/Casino";
-import swoosh from "../assets/swoosh.png";
+import CancelIcon from "@mui/icons-material/Cancel";
+
+// import LocalFloristIcon from "@mui/icons-material/LocalFlorist";
+// import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
+// import BuildIcon from "@mui/icons-material/Build";
+// import VaccinesIcon from "@mui/icons-material/Vaccines";
+// import MonitorHeartIcon from "@mui/icons-material/MonitorHeart";
+// import CasinoIcon from "@mui/icons-material/Casino";
+// import swoosh from "../assets/swoosh.png";
+
+import flowers from "../assets/flowers.jpg";
+import gift from "../assets/gifts.jpg";
+import game from "../assets/game.jpg";
+import maintenance from "../assets/maintenance.jpg";
+import medicine from "../assets/medicine.jpg";
+import eqipment from "../assets/med-device.jpg";
 
 export function ServiceRequestMenu() {
   // Use the Auth0 React hook to handle authentication.
@@ -49,7 +58,8 @@ export function ServiceRequestMenu() {
         <main
           className="flex w-full h-full overflow-y-auto flex-grow justify-center items-center  bg-cover bg-center bg-no-repeat relative"
           style={{
-            backgroundImage: `url(${swoosh})`,
+            // backgroundImage: `url(${swoosh})`,
+            backgroundColor: "white",
             width: "100%",
             height: "100%",
           }}
@@ -65,133 +75,186 @@ export function ServiceRequestMenu() {
           <div className="flex justify-center items-center flex-grow">
             <div
               className="w-fit grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2
-                        justify-items-center justify-center gap-y-20 gap-x-14"
+                        justify-items-center justify-center gap-y-10 gap-x-14"
             >
               <Button
-                className="h-48 w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
-                style={{
-                  backgroundColor: "#009ca6",
+                className="h-72 w-80 shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+                sx={{
+                  border: 8,
+                  borderColor: "#009ca6",
                 }}
                 onClick={() => handleOpen("Flowers")}
               >
-                <Box display="flex" flexDirection="column" alignItems="center">
-                  <LocalFloristIcon
-                    style={{ color: "white", fontSize: "3rem" }}
-                  />
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  alignItems="center"
+                  margin={2}
+                >
                   <span
                     style={{
-                      color: "white",
+                      color: "black",
                       fontWeight: "bold",
                       fontSize: "1.2rem",
                     }}
                   >
                     Flowers
                   </span>
+                  <img
+                    className="overflow-hidden w-52 h-52 border-black border-2 "
+                    src={flowers}
+                    alt={"flowers"}
+                  />
                 </Box>
               </Button>
               <Button
-                className="h-48  w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
-                style={{
-                  backgroundColor: "#009ca6",
+                className="h-72 w-80 shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+                sx={{
+                  border: 8,
+                  borderColor: "#009ca6",
                 }}
                 onClick={() => handleOpen("Gifts")}
               >
-                <Box display="flex" flexDirection="column" alignItems="center">
-                  <CardGiftcardIcon
-                    style={{ color: "white", fontSize: "3rem" }}
-                  />
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  alignItems="center"
+                  margin={2}
+                >
                   <span
                     style={{
-                      color: "white",
+                      color: "black",
                       fontWeight: "bold",
                       fontSize: "1.2rem",
                     }}
                   >
                     Gifts
                   </span>
+                  <img
+                    className="overflow-hidden w-52 h-52 border-black border-2 "
+                    src={gift}
+                    alt={"gifts"}
+                  />
                 </Box>
               </Button>
               <Button
-                className="h-48 w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
-                style={{
-                  backgroundColor: "#009ca6",
+                className="h-72 w-80 shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+                sx={{
+                  border: 8,
+                  borderColor: "#009ca6",
                 }}
                 onClick={() => handleOpen("Entertainment")}
               >
-                <Box display="flex" flexDirection="column" alignItems="center">
-                  <CasinoIcon style={{ color: "white", fontSize: "3rem" }} />
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  alignItems="center"
+                  margin={2}
+                >
                   <span
                     style={{
-                      color: "white",
+                      color: "black",
                       fontWeight: "bold",
                       fontSize: "1.2rem",
                     }}
                   >
                     Entertainment
                   </span>
+                  <img
+                    className="overflow-hidden w-52 h-52 border-black border-2 "
+                    src={game}
+                    alt={"chess"}
+                  />
                 </Box>
               </Button>
-
               <Button
-                className="h-48 w-72 shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
-                style={{
-                  backgroundColor: "#009ca6",
+                className="h-72 w-80 shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+                sx={{
+                  border: 8,
+                  borderColor: "#009ca6",
                 }}
                 onClick={() => handleOpen("Maintenance")}
               >
-                <Box display="flex" flexDirection="column" alignItems="center">
-                  <BuildIcon style={{ color: "white", fontSize: "3rem" }} />
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  alignItems="center"
+                  margin={2}
+                >
                   <span
                     style={{
-                      color: "white",
+                      color: "black",
                       fontWeight: "bold",
                       fontSize: "1.2rem",
                     }}
                   >
                     Maintenance
                   </span>
+                  <img
+                    className="overflow-hidden w-52 h-52 border-black border-2 "
+                    src={maintenance}
+                    alt={"chess"}
+                  />
                 </Box>
               </Button>
               <Button
-                className="h-48 w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
-                style={{
-                  backgroundColor: "#009ca6",
+                className="h-72 w-80 shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+                sx={{
+                  border: 8,
+                  borderColor: "#009ca6",
                 }}
                 onClick={() => handleOpen("Medicine")}
               >
-                <Box display="flex" flexDirection="column" alignItems="center">
-                  <VaccinesIcon style={{ color: "white", fontSize: "3rem" }} />
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  alignItems="center"
+                  margin={2}
+                >
                   <span
                     style={{
-                      color: "white",
+                      color: "black",
                       fontWeight: "bold",
                       fontSize: "1.2rem",
                     }}
                   >
                     Medicine
                   </span>
+                  <img
+                    className="overflow-hidden w-52 h-52 border-black border-2 "
+                    src={medicine}
+                    alt={"chess"}
+                  />
                 </Box>
               </Button>
               <Button
-                className="h-48 w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
-                style={{
-                  backgroundColor: "#009ca6",
+                className="h-72 w-80 shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+                sx={{
+                  border: 8,
+                  borderColor: "#009ca6",
                 }}
                 onClick={() => handleOpen("Medical Equipment")}
               >
-                <Box display="flex" flexDirection="column" alignItems="center">
-                  <MonitorHeartIcon
-                    style={{ color: "white", fontSize: "3rem" }}
-                  />
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  alignItems="center"
+                  margin={2}
+                >
                   <span
                     style={{
-                      color: "white",
+                      color: "black",
                       fontWeight: "bold",
                       fontSize: "1.2rem",
                     }}
                   >
                     Medical Equipment
                   </span>
+                  <img
+                    className="overflow-hidden w-52 h-52 border-black border-2 "
+                    src={eqipment}
+                    alt={"chess"}
+                  />
                 </Box>
               </Button>
 
@@ -204,17 +267,35 @@ export function ServiceRequestMenu() {
                       border: "10px solid #012D5A",
                     }}
                   >
-                    <div className="flex justify-start ">
-                      <Button
-                        style={{
-                          backgroundColor: "#012D5A",
-                          color: "white",
+                    <div className="flex flex-row justify-start mb-8 ">
+                      <Typography
+                        sx={{
+                          color: "#012d5a",
+                          fontSize: "2rem",
+                          fontWeight: "semi-bold",
+                        }}
+                      >
+                        {typeOfService} Request
+                      </Typography>
+
+                      <CancelIcon
+                        sx={{
+                          color: "#012D5A",
+                          fontSize: "3rem",
                           marginLeft: "auto",
                         }}
                         onClick={() => setOpen(false)}
-                      >
-                        Back
-                      </Button>
+                      />
+                      {/*<Button*/}
+                      {/*  style={{*/}
+                      {/*    backgroundColor: "#012D5A",*/}
+                      {/*    color: "white",*/}
+                      {/*    marginLeft: "auto",*/}
+                      {/*  }}*/}
+                      {/*  onClick={() => setOpen(false)}*/}
+                      {/*>*/}
+                      {/*  Back*/}
+                      {/*</Button>*/}
                     </div>
                     <div className="flex items-center justify-center h-full">
                       <ServiceRequestLog typeOfService={typeOfService} />
